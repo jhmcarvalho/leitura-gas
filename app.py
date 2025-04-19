f8133f920a4aeaa087e188f6a1b91a376d945e5e
Jeff <jeferson_funet@hotmail.com>
2025-04-18 21:57:12 -0300

    tentativa de deploy 1

--- app.py
+++ app.py
@@ -120,4 +120,9 @@ def calcular_valor_total(leitura):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    # Executar no modo de desenvolvimento local
+    app.run(debug=True, host='0.0.0.0', port=5000)
+else:
+    # Quando importado por gunicorn ou wsgi (Render)
+    with app.app_context():
+        db.create_all() 
